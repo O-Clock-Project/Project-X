22e5245ff8f4b70e357b81224f31aebbff1afaa7
Noemielej <noemielej@yahoo.fr>
2018-08-20 17:01:10 +0200

    added Form + constraints in validation.yaml

--- undefined/src/Form/UserType.php
+++ undefined/src/Form/UserType.php
@@ -1,30 +1,33 @@
 <?php
+
 namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
+            //->add('username')
             ->add('first_name')
             ->add('last_name')
             ->add('email')
+            // ->add('password', PasswordType::class)
             ->add('pseudo_github')
             ->add('zip')
             ->add('birthday', DateType::class, [
                 'widget' => 'single_text',
             ] )
-
-
         ;
     }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
