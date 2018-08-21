dca642b3ec73a0da525b79500dfeacf1fdd24c48
Milbrin <favouille@gmail.com>
2018-08-22 00:58:38 +0200

    creation of BookmarkVoter

--- undefined/src/Controller/Api/BookmarkController.php
+++ undefined/src/Controller/Api/BookmarkController.php
@@ -102,6 +102,8 @@ class BookmarkController extends AbstractController
     {
         $bookmark = $bookmarkRepo->findOneById($id);
 
+        $this->denyAccessUnlessGranted('edit', $bookmark);
+
         // On crée un formulaire "virtuel" qui va permettre d'utiliser le système de validation des forms Symfony pour checker les données reçues
         // Cf le fichier config/validator/validation.yaml pour les contraintes
         $form = $this->createForm(BookmarkType::class, $bookmark);
--- undefined/src/Services/JWTUtils.php
+++ undefined/src/Services/JWTUtils.php
@@ -18,4 +18,6 @@ class JWTUtils
     
         return $token;
     } 
+
+    
 }
